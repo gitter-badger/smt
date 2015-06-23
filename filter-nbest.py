7ffc8cb2babb2128611158738b36ed19f867a9f1
Ozan Caglayan <ozancag@gmail.com>
2015-06-23 19:10:12 +0200

    filter-nbest: This is now running correctly as expected

--- filter-nbest.py
+++ filter-nbest.py
@@ -3,6 +3,8 @@
 import sys
 import gzip
 
+NBEST_DELIM = " ||| "
+
 if __name__ == "__main__":
     try:
         src = sys.argv[1]
@@ -12,42 +14,95 @@ if __name__ == "__main__":
         print "Usage: %s <src phrase file> <nbest file> <max phrase len>\n" % sys.argv[0]
         sys.exit(1)
 
-    keep_lines = []
-    new_lines = []
-    # First select line numbers with length <= lim
-    with open(src, "rb") as src_data:
-        for ctr, line in enumerate(src_data.readlines()):
-            # It's possible to have empty lines in src phrases..
-            if line[0] != '\n' and len(line.split(" ")) <= lim:
-                keep_lines.append(ctr)
-                new_lines.append(line)
-
-    f = open("%s.%d" % (src, lim), "wb")
-    f.writelines(new_lines)
-    f.close()
-
+    # Handle gzipped n-best files
     open_func = open
     new_nbest_file = "%s.%d" % (nbest, lim)
     if nbest.endswith(".gz"):
         open_func = gzip.open
         new_nbest_file = "%s.%d.gz" % (nbest.split(".gz")[0], lim)
 
-    hyp_start_id = None
+    phrases = []
+    hypotheses = {}
+    n_empty_lines = 0
 
-    new_nbest = []
+    # Collect source phrases
+    with open(src, "rb") as src_data:
+        for line in src_data:
+            line = line.rstrip()
+            if line:
+                phrases.append(line.rstrip() + "\n")
+            else:
+                phrases.append("")
+                n_empty_lines += 1
 
-    # Then process the n-best file to eliminate hypotheses
-    # with source phrases > lim
-    # Discard also translations with phrases > lim
+    n_phrases = len(phrases)
+    print "%s: %d phrases (%d empty lines)" % (src, n_phrases, n_empty_lines)
+
+    print "Reading n-best file."
     with open_func(nbest, "rb") as nbest_data:
         for line in nbest_data:
-            fields = line.rstrip().split(" ||| ")
-            if int(fields[0]) in keep_lines:
-                # It's possible that the translation is bigger than the limit
-                phr_len = len(fields[1].rstrip().split(" "))
-                if phr_len <= lim:
-                    new_nbest.append(line)
+            fields = line.rstrip().split(NBEST_DELIM)
+            sent_id = int(fields[0])
+            try:
+                hypotheses[sent_id].append(fields[1:])
+            except KeyError:
+                hypotheses[sent_id] = [fields[1:]]
+
+    # 1st pass: Filter out long source phrases and their hypotheses
+    print "1st pass from left to right"
+    n_removed_srcp = 0
+    for i, p in enumerate(phrases):
+        l_words = len(p.split())
+        if l_words > lim or l_words == 0:
+            n_removed_srcp += 1
+            phrases[i] = None
+            del hypotheses[i]
+
+    print "Filtered out %d source phrases (%.4f%%)" % (n_removed_srcp,
+            100*float(n_removed_srcp) / n_phrases)
+
+    n_removed_srcp = 0
+
+    print "Total source phrases: %d" % len(phrases)
+
+    print "2nd pass from right to left"
+    for sent_id, hypos in hypotheses.items():
+        for i, sent in enumerate(hypos):
+            sent_len = len(sent[0].rstrip().split(" "))
+            if sent_len > lim:
+                del hypos[i]
+
+        if len(hypos) == 0:
+            n_removed_srcp += 1
+            # No sentences left for this hyp, remove it
+            del hypotheses[sent_id]
+            # Mark corresponding source phrase as None also
+            phrases[sent_id] = None
+
+    print "Filtered out %d further source phrases" % n_removed_srcp
+    print "%d final hypotheses" % sum([len(k) for k in hypotheses.values()])
+
+    # These should be equal
+    final_phrases = [p for p in phrases if p]
+    assert(len(final_phrases) == len(hypotheses.keys()))
+
+    # Dump files
+    f_src = open("%s.%d" % (src, lim), "wb")
+    f_src.writelines(final_phrases)
+    f_src.close()
+
+    del final_phrases, phrases
+
+    line_ctr = 0
 
     f_out = open_func(new_nbest_file, "wb")
-    f_out.writelines(new_nbest)
+    print "Dumping nbest file..."
+    for sent_id in sorted(hypotheses.keys()):
+        nbest_lines = []
+        for s in hypotheses[sent_id]:
+            s.insert(0, str(line_ctr))
+            nbest_lines.append(NBEST_DELIM.join(s) + "\n")
+        line_ctr += 1
+        f_out.writelines(nbest_lines)
+
     f_out.close()
